d393067c9eb78a9e7dafd8657d6cfbcafa89b4d0
ketan akbari <ketan.akbari123@gmail.com>
2022-01-04 13:22:35 +0530

    home page Ui fixes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,50 +3,50 @@
     <div class="container">
       <div class="mx-4 dashboard-page-mt">
         <div class="row">
-          <div class="col-lg-3 col-md-4 col-sm-2 card-mb">
+          <div class="col-6 card-mb">
             <div class="img-a dashboard-box-widget" @click="redirectToPath('/category/CONFIGURAZIONI')">
               CONFIGURAZIONI
             </div>
           </div>
 
-          <div class="col-lg-3 col-md-4 col-sm-2 card-mb">
-            <div class="img-b dashboard-box-widget" @click="redirectToPath('/nsg-core-indicators')">
+          <div class="col-6 card-mb">
+            <div class="img-a dashboard-box-widget" @click="redirectToPath('/nsg-core-indicators')">
               NSG – INDICATORI CORE
             </div>
           </div>
 
-          <div class="col-lg-3 col-md-4 col-sm-2 card-mb">
-            <div class="img-c dashboard-box-widget" @click="redirectToPath('/management-area')">
+          <div class="col-6 card-mb">
+            <div class="img-a dashboard-box-widget" @click="redirectToPath('/management-area')">
               AREA GESTIONALE – DCA 67/2019
             </div>
           </div>
 
-          <div class="col-lg-3 col-md-4 col-sm-2 card-mb" @click="redirectToPath('/pharmacy-area')">
+          <div class="col-6 card-mb" @click="redirectToPath('/pharmacy-area')">
             <div class="img-a  dashboard-box-widget">
               AREA FARMACIA
             </div>
           </div>
 
-          <div class="col-lg-3 col-md-4 col-sm-2 card-mb" @click="redirectToPath('/pne')">
-            <div class="img-c dashboard-box-widget">
+          <div class="col-6 card-mb" @click="redirectToPath('/pne')">
+            <div class="img-a dashboard-box-widget">
               PNE
             </div>
           </div>
 
-          <div class="col-lg-3 col-md-4 col-sm-2 card-mb" @click="redirectToPath('/prevention')">
+          <div class="col-6 card-mb" @click="redirectToPath('/prevention')">
             <div class="img-a dashboard-box-widget">
               PREVENZIONE
             </div>
           </div>
 
-          <div class="col-lg-3 col-md-4 col-sm-2 card-mb" @click="redirectToPath('/status-of-payment')">
-            <div class="img-b dashboard-box-widget">
+          <div class="col-6 card-mb" @click="redirectToPath('/status-of-payment')">
+            <div class="img-a dashboard-box-widget">
               STATO DEI PAGAMENTI
             </div>
           </div>
 
-          <div class="col-lg-3 col-md-4 col-sm-2 card-mb" @click="redirectToPath('/staff')">
-            <div class="img-c dashboard-box-widget">
+          <div class="col-6 card-mb" @click="redirectToPath('/staff')">
+            <div class="img-a dashboard-box-widget">
               PERSONALE
             </div>
           </div>
@@ -88,6 +88,7 @@ export default {
   text-shadow: 2px 2px lightgray;
   display: flex;
   align-items: center;
+  justify-content: center;
 }
 
 .dashboard-page-mt {
@@ -95,11 +96,13 @@ export default {
 }
 
 .card-mb {
-  margin-bottom: 60px;
+  margin-bottom: 48px;
 }
 
 .img-a {
-  background-image: url('../assets/image/prevenzione.png');
+  //background-image: url('../assets/image/prevenzione.png');
+  //background-image: url('../assets/image/contesto_ed_equita.png');
+  background-image: url('../assets/image/assistenza-ospedaliera.png');
   background-repeat: no-repeat;
   background-size: auto;
 }
