b6bb7548ac9be56ec6d068ac8a6c25b26cbf3e40
Tarun <tgchauhan1711@gmail.com>
2022-02-19 19:42:32 +0530

    fix ui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,49 +5,57 @@
         <div class="row">
           <div class="col-6 card-mb">
             <div class="dashboard-box-widget configurazioni" @click="redirectToPath('/category/CONFIGURAZIONI')">
-              <v-img :src="require('../assets/image/dash-configurazione.png')" /> CONFIGURAZIONI
+              <div class="icon-block"><v-img :src="require('../assets/image/dash-configurazione.png')" /></div>
+              <div class="text-block">CONFIGURAZIONI</div>
             </div>
           </div>
 
           <div class="col-6 card-mb">
             <div class="dashboard-box-widget nsg-indicatori-core" @click="redirectToPath('/nsg-core-indicators')">
-              <v-img :src="require('../assets/image/dash-nsg.png')" /> NSG – INDICATORI CORE
+              <div class="icon-block"><v-img :src="require('../assets/image/dash-nsg.png')" /></div>
+              <div class="text-block">NSG – INDICATORI CORE</div>
             </div>
           </div>
 
           <div class="col-6 card-mb">
             <div class="dashboard-box-widget area-gestionale" @click="redirectToPath('/management-area')">
-              <v-img :src="require('../assets/image/dash-area-gestionale.png')" /> AREA GESTIONALE – DCA 67/2019
+              <div class="icon-block"><v-img :src="require('../assets/image/dash-area-gestionale.png')" /></div>
+              <div class="text-block">AREA GESTIONALE – DCA 67/2019</div>
             </div>
           </div>
 
           <div class="col-6 card-mb" @click="redirectToPath('/pharmacy-area')">
             <div class=" dashboard-box-widget area-farmacia">
-              <v-img :src="require('../assets/image/dash-area-farmacia.png')" /> AREA FARMACIA
+              <div class="icon-block"><v-img :src="require('../assets/image/dash-area-farmacia.png')" /></div>
+              <div class="text-block">AREA FARMACIA</div>
             </div>
           </div>
 
           <div class="col-6 card-mb" @click="redirectToPath('/pne')">
             <div class="dashboard-box-widget pne">
-              <v-img :src="require('../assets/image/dash-pne.png')" /> PNE
+              <div class="icon-block"><v-img :src="require('../assets/image/dash-pne.png')" /></div>
+              <div class="text-block">PNE</div>
             </div>
           </div>
 
           <div class="col-6 card-mb" @click="redirectToPath('/prevention')">
             <div class="dashboard-box-widget prevenzione">
-              <v-img :src="require('../assets/image/dash-prevenzione.png')" /> PREVENZIONE
+              <div class="icon-block"><v-img :src="require('../assets/image/dash-prevenzione.png')" /></div>
+              <div class="text-block">PREVENZIONE</div>
             </div>
           </div>
 
           <div class="col-6 card-mb" @click="redirectToPath('/status-of-payment')">
             <div class="dashboard-box-widget stato-dei-pagamenti">
-              <v-img :src="require('../assets/image/dash-pagamenti.png')" /> STATO DEI PAGAMENTI
+              <div class="icon-block"><v-img :src="require('../assets/image/dash-pagamenti.png')" /></div>
+              <div class="text-block">STATO DEI PAGAMENTI</div>
             </div>
           </div>
 
           <div class="col-6 card-mb" @click="redirectToPath('/staff')">
             <div class="dashboard-box-widget persoale">
-              <v-img :src="require('../assets/image/dash-personale.png')" /> PERSONALE
+              <div class="icon-block"><v-img :src="require('../assets/image/dash-personale.png')" /></div>
+              <div class="text-block">PERSONALE</div>
             </div>
           </div>
 
@@ -79,7 +87,7 @@ export default {
   border-radius: 15px;
   padding: 25px;
   height: 100%;
-  min-height: 155px;
+  min-height: 180px;
   font-weight: bolder;
   font-size: 22px;
   overflow: hidden;
