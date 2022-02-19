2b691805a60bd8af25bb21c4bec279c9c49df4c8
Tarun <tgchauhan1711@gmail.com>
2022-02-19 16:19:17 +0530

    In the home page please for the dashboards use given design

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,50 +4,50 @@
       <div class="mx-4 dashboard-page-mt">
         <div class="row">
           <div class="col-6 card-mb">
-            <div class="dashboard-box-widget" @click="redirectToPath('/category/CONFIGURAZIONI')">
-              CONFIGURAZIONI
+            <div class="dashboard-box-widget configurazioni" @click="redirectToPath('/category/CONFIGURAZIONI')">
+              <v-img :src="require('../assets/image/dash-configurazione.png')" /> CONFIGURAZIONI
             </div>
           </div>
 
           <div class="col-6 card-mb">
-            <div class="dashboard-box-widget" @click="redirectToPath('/nsg-core-indicators')">
-              NSG – INDICATORI CORE
+            <div class="dashboard-box-widget nsg-indicatori-core" @click="redirectToPath('/nsg-core-indicators')">
+              <v-img :src="require('../assets/image/dash-nsg.png')" /> NSG – INDICATORI CORE
             </div>
           </div>
 
           <div class="col-6 card-mb">
-            <div class="dashboard-box-widget" @click="redirectToPath('/management-area')">
-              AREA GESTIONALE – DCA 67/2019
+            <div class="dashboard-box-widget area-gestionale" @click="redirectToPath('/management-area')">
+              <v-img :src="require('../assets/image/dash-area-gestionale.png')" /> AREA GESTIONALE – DCA 67/2019
             </div>
           </div>
 
           <div class="col-6 card-mb" @click="redirectToPath('/pharmacy-area')">
-            <div class=" dashboard-box-widget">
-              AREA FARMACIA
+            <div class=" dashboard-box-widget area-farmacia">
+              <v-img :src="require('../assets/image/dash-area-farmacia.png')" /> AREA FARMACIA
             </div>
           </div>
 
           <div class="col-6 card-mb" @click="redirectToPath('/pne')">
-            <div class="dashboard-box-widget">
-              PNE
+            <div class="dashboard-box-widget pne">
+              <v-img :src="require('../assets/image/dash-pne.png')" /> PNE
             </div>
           </div>
 
           <div class="col-6 card-mb" @click="redirectToPath('/prevention')">
-            <div class="dashboard-box-widget">
-              PREVENZIONE
+            <div class="dashboard-box-widget prevenzione">
+              <v-img :src="require('../assets/image/dash-prevenzione.png')" /> PREVENZIONE
             </div>
           </div>
 
           <div class="col-6 card-mb" @click="redirectToPath('/status-of-payment')">
-            <div class="dashboard-box-widget">
-              STATO DEI PAGAMENTI
+            <div class="dashboard-box-widget stato-dei-pagamenti">
+              <v-img :src="require('../assets/image/dash-pagamenti.png')" /> STATO DEI PAGAMENTI
             </div>
           </div>
 
           <div class="col-6 card-mb" @click="redirectToPath('/staff')">
-            <div class="dashboard-box-widget">
-              PERSONALE
+            <div class="dashboard-box-widget persoale">
+              <v-img :src="require('../assets/image/dash-personale.png')" /> PERSONALE
             </div>
           </div>
 
@@ -76,11 +76,10 @@ export default {
 <style lang="scss" scoped>
 .dashboard-box-widget {
   background-size: cover;
-  border-radius: 10px;
-  transform: scale(1.019, 1.5);
+  border-radius: 15px;
   padding: 25px;
   height: 100%;
-  min-height: 100px;
+  min-height: 155px;
   font-weight: bolder;
   font-size: 22px;
   overflow: hidden;
@@ -91,14 +90,10 @@ export default {
   justify-content: center;
 
   background: linear-gradient(to bottom, #27b3b8 0%, #f3f4f4 100%);
-  border: 1px solid #27b3b8 ;
+  border: 2px solid #27b3b8 ;
 }
 
 .dashboard-page-mt {
   margin-top: 48px;
-}
-
-.card-mb {
-  margin-bottom: 48px;
 }
 </style>
